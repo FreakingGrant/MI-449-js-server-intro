b85588988f1d213d1413face01fc9024f8389234
FreakingGrant <youngsgr@msu.edu>
2020-02-27 13:18:06 -0500

    Created the node web server to bring the pages needed at the appropriate URLs

---
+++ server.js
@@ -0,0 +1,64 @@
+// Require Node's http module and assign it to a variable
+var http = require('http')
+
+// Create a new server that just says "Hi!!" at every route
+var server = http.createServer(function (request, response) {
+  if (request.url === '/') {
+    response.end( 
+      '<h1>Sup dude</h1>' +
+      '<img src="https://i.imgur.com/jKhQJVH.jpg" alt="Waving hi">'
+    )
+  } else if (request.url === '/random-joke') {
+    response.end(
+      randomJoke() +
+      "<a href='/'>Back to home</a>"
+    )
+  } else if (request.url === '/cuteness') {
+    response.end(
+      "<a href='/'>Back to home</a>" +
+      '<p><img src="https://i.imgur.com/MQHYB.jpg" alt="Super cute cat"></p>'
+    )
+  } 
+  else {
+    response.end(
+      '<h1>Error 404: Page not found</h1>' +
+      '<p>Son of Anton\'s machine learning algorithm must have deleted the page. It\'s still learning. We\'ll get to fixing that eventually</p>' +
+      "<a href='/'>Back to home</a>"
+    )
+  }
+})
+
+// Listen on port 8080, so that we can reach he app at localhost:8080
+var port = process.env.PORT || 8080
+server.listen(port)
+
+// Output a friendly message to the terminal
+console.log('Server running at http://localhost:' + port + '/')
+
+var randomJoke = function () {
+  var html = '<p>'
+  var random = Math.floor((Math.random() * 3))
+
+  if (random === 0) {
+    html += 'Knock knock' +
+            '<p>Who\'s there?</p>' +
+            '<p>Suck.</p>' +
+            '<p>Suck who?</p>' +
+            '<p>Sucks to be you!</p>'
+  } else if (random === 1) {
+    html += 'Knock knock' +
+            '<p>Who\'s there?</p>' +
+            '<p>Tank</p>' +
+            '<p>Tank who?</p>' +
+            '<p>You\'re welcome</p>'
+  } else {
+    html += 'Knock knock' +
+            '<p>Who\'s there?</p>' +
+            '<p>Beats.</p>' +
+            '<p>Beats who?</p>' +
+            '<p>Beats me.</p>'
+  }
+
+  html += '</p>'
+  return html
+}
